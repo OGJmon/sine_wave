b7da8954ce997f62f0fc1267be68eb5a04caae0f
Jason <jason.halliday@maine.edu>
2022-11-14 14:51:55 -0500

    Update program.c to compile SineGenerator.c to the AVR

--- program.c
+++ program.c
@@ -3,7 +3,7 @@
 
 int main() {
 
-	system("avr-gcc -mmcu=atmega88 fixedpoint.c"); //compile avrcode to a.hex
+	system("avr-gcc -mmcu=atmega88 SineGenerator.c"); //compile avrcode to a.hex
 	system("avr-objcopy -j .text -j .data -O ihex a.out a.hex");
 	system("sudo avrdude -C ~/avrdude_gpio.conf -c pi_1 -p atmega88p -U lfuse:w:0xe2:m -U hfuse:w:0xdf:m -U efuse:w:0xf9:m");
 	system("sudo avrdude -C ~/avrdude_gpio.conf -c pi_1 -p atmega88p -U flash:w:a.hex:i");
